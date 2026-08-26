d16bd08a963439c100e53f8c971073da5403cb27
Raul <raullmdsantos@gmail.com>
2026-08-26 20:23:47 -0300

    Adds text, icon and badge components

--- projetos/01/todo/src/App.tsx
+++ projetos/01/todo/src/App.tsx
@@ -1,5 +1,44 @@
+import Text from "./components/text";
+
+import Icon from "./components/icon";
+import XIcon from "./assets/icons/X-Regular.svg?react";
+import SpinnerIcon from "./assets/icons/spinner.svg?react";
+import PlusIcon from "./assets/icons/Plus-Regular.svg?react";
+import TrashIcon from "./assets/icons/Trash-Regular.svg?react";
+import CheckIcon from "./assets/icons/Check-Regular.svg?react";
+import PencilIcon from "./assets/icons/PencilSimple-Regular.svg?react";
+import Badge from "./components/badge";
+
 function App() {
-  return <div className="text-pink-dark">Olá mundo!</div>;
+  return (
+    <div className="grid gap-3">
+      <div className="flex flex-col gap-2">
+        <Text variant={"body-sm-bold"} className="text-pink-base">
+          Olá mundo!
+        </Text>
+        <Text variant={"body-md"} className="text-pink-base">
+          Olá mundo!
+        </Text>
+        <Text variant={"body-md-bold"} className="text-pink-base">
+          Olá mundo!
+        </Text>
+      </div>
+
+      <div className="flex gap-1">
+        <Icon svg={TrashIcon} className="fill-green-base" />
+        <Icon svg={CheckIcon} className="fill-green-base" />
+        <Icon svg={PencilIcon} className="fill-green-base" />
+        <Icon svg={PlusIcon} className="fill-green-base" />
+        <Icon svg={XIcon} className="fill-green-base" />
+        <Icon svg={SpinnerIcon} className="fill-green-base" animate />
+      </div>
+
+      <div>
+        <Badge variant={"secondary"}>5</Badge>
+        <Badge variant={"primary"}>2 de 5</Badge>
+      </div>
+    </div>
+  );
 }
 
 export default App;
