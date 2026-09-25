fc715f69125ff1245639c4c7ef50c22d16f672e6
Raul <raullmdsantos@gmail.com>
2026-09-25 17:19:38 -0300

    Aulas

--- 001/classroom/src/App.tsx
+++ 001/classroom/src/App.tsx
@@ -1,20 +1,32 @@
 import "./global.css";
+import React, { useEffect } from "react";
 import { Button } from "./button";
 import styles from "./app.module.css";
 import useMessage from "./hooks/useMessage";
 
 function App() {
+  const [count, setCount] = React.useState(0);
+
   const user = {
     name: "Raul",
   };
 
   const { show } = useMessage({ name: user.name });
 
+  useEffect(() => {
+    if (count > 0) {
+      console.log(`O valor mudou para ${count}`);
+    }
+  }, [count]);
+
   return (
     <div className={styles.container}>
-      <Button name="Adicionar" onClick={() => show("Olá mundo!!")} />
-      <span>0</span>
-      <Button name="Deletar" onClick={() => alert("Deletar")} />
+      <Button name="Adicionar" onClick={() => setCount((c) => c + 1)} />
+      <span>{count}</span>
+      <Button
+        name="Deletar"
+        onClick={() => setCount((c) => Math.max(0, c - 1))}
+      />
     </div>
   );
 }
